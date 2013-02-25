b1b150787641eda31fb6d5caee793c0b02996a8d
dukebanerjee <dbanerje@sbcglobal.net>
2013-02-24 23:24:36 -0600

    Refactor and cleanup

---
+++ src/main/java/examples/calculator/engine/Operation.java
@@ -0,0 +1,11 @@
+package examples.calculator.engine;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: duke
+ * Date: 2/24/13
+ * Time: 11:08 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public interface Operation {
+}
