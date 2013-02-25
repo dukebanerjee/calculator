3cdba84004307f0b79676ed90ef38b8d8bcf6c0e
dukebanerjee <dbanerje@sbcglobal.net>
2013-02-24 23:01:37 -0600

    Implement addition

---
+++ src/main/java/examples/calculator/engine/Calculator.java
@@ -0,0 +1,28 @@
+package examples.calculator.engine;
+
+public class Calculator {
+    private int value;
+    private int operand;
+    private Object operation;
+
+    public static Object Addition = new Object();
+    public static Object Equals = new Object();
+
+    public void setValue(int value) {
+        this.operand = this.value;
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setOperation(Object operation) {
+        if(operation == Equals) {
+            if(this.operation == Addition) {
+                this.value = this.operand + this.value;
+            }
+        }
+        this.operation = operation;
+    }
+}
---
+++ src/test/java/examples/calculator/unit/engine/CalculatorTest.java
@@ -0,0 +1,33 @@
+package examples.calculator.unit.engine;
+
+import examples.calculator.engine.Calculator;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+public class CalculatorTest {
+    private Calculator calculator;
+
+    @Before
+    public void setup() {
+        calculator = new Calculator();
+    }
+
+    @Test
+    public void shouldReturnEnteredValueWhenFirstOperatorIsPressed() {
+        calculator.setValue(3);
+        calculator.setOperation(Calculator.Addition);
+        assertThat(calculator.getValue(), equalTo(3));
+    }
+
+    @Test
+    public void shouldEvaluateOperationWhenEqualsIsPressed() {
+        calculator.setValue(3);
+        calculator.setOperation(Calculator.Addition);
+        calculator.setValue(2);
+        calculator.setOperation(Calculator.Equals);
+        assertThat(calculator.getValue(), equalTo(5));
+    }
+}
